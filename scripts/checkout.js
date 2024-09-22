6a140d83271cfa91300fade680fd330f2fbd0760
irfankabir07 <irfanulhoquekabir07@gmail.com>
2024-09-22 21:34:01 +0600

    Upload JavaScript Amazon Clone Project
    This commit adds the complete JavaScript-based Amazon clone project, featuring shopping cart functionality, order summary updates, and dynamic product displays.

---
+++ scripts/checkout.js
@@ -0,0 +1,81 @@
+import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
+import { loadCart, loadCartFetch } from '../data/cart.js';
+//import '../data/cart-class.js';
+//import '../data/car.js';
+//import '../data/backend-practice.js';
+
+
+async function loadPage() {
+  try {
+    //throw 'error1';
+
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ]);
+
+  } catch (error) {
+    console.log('unexpected error. Please try again later.');
+  }
+  
+
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+
+/*
+Promise.all([
+  loadProductsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+
+]).then((values) => {
+  console.log(values);
+
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+/*
+new Promise((resolve) => {
+  loadProducts(()=>{
+    resolve('value 1');
+  });
+
+}).then( (value) => {
+  console.log(value);
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  });
+
+}).then(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
